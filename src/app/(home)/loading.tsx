5befb6060a1f6b7d54ff238c4e6a016d956afdd8
Thefederico <federico_upegui.dev@outlook.com>
2024-06-29 00:13:33 -0500

    tailwind config

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,7 @@
+export default function loading() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold text-pretty">Loading...</h1>
+    </div>
+  );
+}
